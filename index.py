2f92c48931395d2a067f96ca30352c42cea96e5d
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2024-06-08 22:05:30 +0400

    Fixed Background

--- index.py
+++ index.py
@@ -2,13 +2,13 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-class window(QWidget):
+class window(QMainWindow):
    def __init__(self, parent = None):
       super(window, self).__init__(parent)
       self.setFixedSize(1120,560)
-      self.setWindowTitle("Housify")   
-      self.setStyleSheet("background:url('./src/background.png') center")
-   
+      self.setWindowTitle("Housify")  
+      self.setStyleSheet('background:url("./src/background.png") center no-repeat') 
+
 def main():
    app = QApplication(sys.argv)
    ex = window()
