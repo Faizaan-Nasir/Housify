40071cd10cf91ef57553250454fda20db6526cbf
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2024-06-09 00:33:32 +0400

    Added Font Dependencies

--- index.py
+++ index.py
@@ -1,18 +1,29 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFontDatabase
+
+# main window
 class window(QMainWindow):
    def __init__(self, parent = None):
       super(window, self).__init__(parent)
       self.setFixedSize(1120,560)
       self.setWindowTitle("Housify")  
       self.setStyleSheet('background:url("./src/background.png") center no-repeat') 
+      self.mainTitle=QLabel("HOUSIFY",self)
+      self.mainTitle.setFixedWidth(1120)
+      self.mainTitle.setFixedHeight(50)
+      self.mainTitle.move(0,200)
+      self.mainTitle.setAlignment(QtCore.Qt.AlignCenter)
+      self.mainTitle.setStyleSheet("font-family: Paytone One; background: transparent; font-size:45px")
 
 def main():
    app = QApplication(sys.argv)
+   QFontDatabase.addApplicationFont('./src/fonts/Paytone_One/PaytoneOne-Regular.ttf')
+   QFontDatabase.addApplicationFont('./src/fonts/Poppins/Poppins-Regular.ttf')
    ex = window()
    ex.show()
    sys.exit(app.exec_())
+   
 if __name__ == '__main__':
    main()
