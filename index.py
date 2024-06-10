c22314ed54c70e16bafe6d8a6ef8005723e12ee6
Faizaan-Nasir <faizaannasir06@gmail.com>
2024-06-10 15:00:40 +0400

    Join Game Window created

--- index.py
+++ index.py
@@ -3,6 +3,49 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFontDatabase , QPixmap , QPalette , QBrush
 
+# join a game window
+class joinGameWindow(QWidget):
+   def __init__(self):
+      super().__init__()
+      self.setFixedSize(1120,560)
+      self.setWindowTitle("Housify - Join a Game")
+      pixmap = QPixmap('./src/background.png')
+      palette = self.palette()
+      palette.setBrush(QPalette.Background, QBrush(pixmap))
+      self.setPalette(palette)
+
+      # HOUSIFY
+      self.mainTitle=QLabel("HOUSIFY",self)
+      self.mainTitle.setFixedSize(1120,50)
+      self.mainTitle.move(0,210)
+      self.mainTitle.setAlignment(QtCore.Qt.AlignCenter)
+      self.mainTitle.setStyleSheet("font-family: Paytone One; background: transparent; font-size:60px; color: black;")
+
+      # Game Code
+      self.enterGameCode=QLineEdit(self)
+      self.enterGameCode.setStyleSheet("font-family: Poppins; font-size: 21px; background: #D7D7D7; border: 2px solid black;")
+      self.enterGameCode.setFixedSize(200,55)
+      self.enterGameCode.move(340,310)
+      self.enterGameCode.setAlignment(QtCore.Qt.AlignCenter)
+      self.enterGameCode.setPlaceholderText('Game Code')
+      self.enterGameCode.setFocusPolicy(0x2)
+
+      # Submit Button
+      self.submitGameCode=QPushButton('Submit',self)
+      self.submitGameCode.setStyleSheet('''QPushButton{
+                                  font-family: Poppins; 
+                                  font-size: 21px; 
+                                  background: #69B1F4; 
+                                  border: 2px solid black;
+                                  color: black;
+                                  }
+                                  QPushButton::hover{
+                                  background: #63a9eb;
+                                  }''')
+      self.submitGameCode.setFixedSize(200,55)
+      self.submitGameCode.move(580,310)
+      #self.submitGameCode.clicked.connect()
+
 # main window
 class mainWindow(QWidget):
    def __init__(self):
@@ -14,13 +57,13 @@ class mainWindow(QWidget):
       palette.setBrush(QPalette.Background, QBrush(pixmap))
       self.setPalette(palette)
       
-      # Title
+      # HOUSIFY
       self.mainTitle=QLabel("HOUSIFY",self)
       self.mainTitle.setFixedSize(1120,50)
       self.mainTitle.move(0,210)
       self.mainTitle.setAlignment(QtCore.Qt.AlignCenter)
-      self.mainTitle.setStyleSheet("font-family: Paytone One; background: transparent; font-size:60px")
-
+      self.mainTitle.setStyleSheet("font-family: Paytone One; background: transparent; font-size:60px; color: black;")
+      
       # Host a Game Button
       self.hostGame=QPushButton('Host a Game',self)
       self.hostGame.setStyleSheet('''QPushButton{
@@ -28,6 +71,7 @@ class mainWindow(QWidget):
                                   font-size: 21px; 
                                   background: #69B1F4; 
                                   border: 2px solid black;
+                                  color: black;
                                   }
                                   QPushButton::hover{
                                   background: #63a9eb;
@@ -43,20 +87,27 @@ class mainWindow(QWidget):
                                   font-size: 21px; 
                                   background: #69B1F4; 
                                   border: 2px solid black;
+                                  color: black;
                                   }
                                   QPushButton::hover{
                                   background: #63a9eb;
                                   }''')
       self.joinGame.setFixedSize(200,55)
       self.joinGame.move(580,310)
-      #self.joinGame.clicked.connect()
+      self.joinGame.clicked.connect(self.joinGameButton)
 
       # Copyrights
       self.copyright=QLabel('© All rights reserved, Housify Pvt Ltd, 2024',self)
-      self.copyright.setStyleSheet('font-family: Poppins; font-size: 10px; background: transparent')
+      self.copyright.setStyleSheet('font-family: Poppins; font-size: 10px; background: transparent; color: black;')
       self.copyright.setFixedWidth(560)
       self.copyright.setAlignment(QtCore.Qt.AlignCenter)
       self.copyright.move(280,390)
+
+   # function to show joinGameWindow
+   def joinGameButton(self):
+      self.newWin = joinGameWindow()
+      self.newWin.show()
+      self.hide()
 
 def main():
    app = QApplication(sys.argv)
