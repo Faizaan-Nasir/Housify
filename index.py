dfb2288a88467a6ab92be89a20a0e85f4d1332ee
Faizaan-Nasir <faizaannasir06@gmail.com>
2024-06-14 00:53:57 +0400

    Join Game Window created

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFontDatabase , QPixmap , QPalette , QBrush
+import random
+import pyperclip
 
 # main window
 class mainWindow(QWidget):
@@ -94,7 +96,7 @@ class joinGameWindow(QWidget):
 
       # Game Code
       self.enterGameCode=QLineEdit(self)
-      self.enterGameCode.setStyleSheet("color: black;font-family: Poppins; font-size: 21px; background: #D7D7D7; border: 2px solid black;")
+      self.enterGameCode.setStyleSheet("color: black; font-family: Poppins; font-size: 21px; background: #D7D7D7; border: 2px solid black;")
       self.enterGameCode.setFixedSize(200,55)
       self.enterGameCode.move(340,310)
       self.enterGameCode.setAlignment(QtCore.Qt.AlignCenter)
@@ -121,6 +123,7 @@ class joinGameWindow(QWidget):
 class hostGameWindow(QWidget):
    def __init__(self):
       super().__init__()
+      self.setFixedSize(1120,560)
       self.setWindowTitle('Housify - Host a Game')
       pixmap = QPixmap('./src/background_host.png')
       palette = self.palette()
@@ -131,15 +134,77 @@ class hostGameWindow(QWidget):
    def MainUI(self):
       # HOUSIFY
       self.mainTitle=QLabel("HOUSIFY",self)
-      self.mainTitle.setFixedSize(1120,52)
-      self.mainTitle.move(0,210)
+      self.mainTitle.setFixedSize(1120,47)
+      self.mainTitle.move(0,165)
       self.mainTitle.setAlignment(QtCore.Qt.AlignCenter)
-      self.mainTitle.setStyleSheet("font-family: Paytone One; background: transparent; font-size:60px; color: black;")
+      self.mainTitle.setStyleSheet("font-family: Poppins; font-weight: 900; background: transparent; font-size:35px; color: black;")
+
+      # Game Code Label
+      self.gameCodeLabelGame=QLabel('Game',self)
+      self.gameCodeLabelGame.setStyleSheet("color: black; font-family: Poppins; font-weight: 700; font-size: 22px;")
+      self.gameCodeLabelGame.move(393,230)
+      self.gameCodeLabelCode=QLabel('Code',self)
+      self.gameCodeLabelCode.setStyleSheet("color: black; font-family: Poppins; font-weight: 700; font-size: 22px;")
+      self.gameCodeLabelCode.move(397,255)
+
+      # Colon
+      self.colon=QLabel(':',self)
+      self.colon.setStyleSheet("color: black; font-family: Poppins; font-weight: 900; font-size: 65px;")
+      self.colon.move(490,210)
+
+      # Game Code
+      self.newGameCode=str(random.randint(10000,99999))
+      self.gameCode=QLabel(self.newGameCode,self)
+      self.gameCode.setStyleSheet("color: black; font-family: Poppins; font-weight: 900; font-size: 62px;")
+      self.gameCode.move(535,217)
+
+      # Star Game Button
+      self.startGame=QPushButton('Start Game',self)
+      self.startGame.setStyleSheet('''QPushButton{
+                                 font-family: Poppins; 
+                                 font-size: 18px; 
+                                 background: #69B1F4; 
+                                 border: 2px solid black;
+                                 color: black;
+                                 }
+                                 QPushButton::hover{
+                                 background: #63a9eb;}''')
+      self.startGame.setFixedSize(200,55)
+      self.startGame.move(340,325)
+      #self.startGame.clicked.connect()
+
+      # Copy to Clipboard Function
+      def c2cbFunc(self):
+         pyperclip.copy(self.newGameCode)
+         self.copied=QLabel('Copied to Clipboard',self)
+         self.copied.setStyleSheet('font-family: poppins; font-size: 12px; color: #D2626E;')
+         self.copied.setFixedWidth(1120)
+         self.copied.setAlignment(QtCore.Qt.AlignCenter)
+         self.copied.move(0,390)
+         self.copied.show()
+         QtCore.QTimer.singleShot(5000,lambda: self.copied.hide())
+
+      # Copy to Clipboard Button
+      self.c2cb=QPushButton('Copy to Clipboard',self)
+      self.c2cb.setStyleSheet('''QPushButton{
+                                 font-family: Poppins; 
+                                 font-size: 18px; 
+                                 background: #69B1F4; 
+                                 border: 2px solid black;
+                                 color: black;
+                                 }
+                                 QPushButton::hover{
+                                 background: #63a9eb;}''')
+      self.c2cb.setFixedSize(200,55)
+      self.c2cb.move(580,325)
+      self.c2cb.clicked.connect(lambda: c2cbFunc(self))
 
 def main():
    app = QApplication(sys.argv)
    QFontDatabase.addApplicationFont('./src/fonts/Paytone_One/PaytoneOne-Regular.ttf')
    QFontDatabase.addApplicationFont('./src/fonts/Poppins/Poppins-Regular.ttf')
+   QFontDatabase.addApplicationFont('./src/fonts/Poppins/Poppins-ExtraBold.ttf')
+   QFontDatabase.addApplicationFont('./src/fonts/Poppins/Poppins-SemiBold.ttf')
    ex = mainWindow()
    ex.show()
    sys.exit(app.exec_())
