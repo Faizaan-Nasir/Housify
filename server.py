f6abfd2c4d7f79d39b76b87b306d48cd85011f5a
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2024-07-09 23:58:29 +0400

    WE HAVE A WORKING MODEL

--- server.py
+++ server.py
@@ -1,7 +1,28 @@
 import socket
 import pickle
 import select
-from connection import Game
+
+class Game : 
+
+    def __init__(self, code, hostname, host_client) : 
+        self.code = code
+        self.hostname = hostname
+        self.host_client = host_client
+        self.players = {}
+
+    def add_player(self, name, socket) :
+        self.players[name] = socket
+    
+    def remove_player(self, socket) : 
+        if self.host_client == socket : 
+            return 2
+        elif socket in list(self.players.values()) : 
+            for n, c in self.players.items() : 
+                if c == socket : 
+                    del self.players[n]
+                    return 1
+        else : 
+            return 0
 
 class Server : 
     
